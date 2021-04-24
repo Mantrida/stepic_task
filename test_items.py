8fd24071bcd28b08c5ec5443d8a01a9dfb0ecde0
Mantrida <Lexx47@mail.ru>
2021-04-24 19:31:56 +0300

    relocation link

--- test_items.py
+++ test_items.py
@@ -1,8 +1,5 @@
 import time
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-
-
 class TestProductPage:
 
     def test_button_add_to_basket_is_visible(self, browser):
@@ -10,11 +7,12 @@ class TestProductPage:
          содержит кнопку добавления в корзину
         """
         # Открываем страницу товара
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
 
         # Установлено время принудительной задержки браузера
         # после открытия страницы, для визуальной проверки языка открытой страницы
-        time.sleep(20)
+        time.sleep(10)
 
         # Проверяем наличие кнопки добавления товара в корзину
         assert browser.find_element_by_css_selector("button.btn-add-to-basket")
